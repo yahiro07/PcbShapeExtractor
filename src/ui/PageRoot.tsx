e1a9331e2196272e5fecce9e020a37963361ecf0
yahiro <yahiro07@users.noreply.github.com>
2022-09-10 18:24:14 +0900

    ページの表示を調整

--- src/ui/PageRoot.tsx
+++ src/ui/PageRoot.tsx
@@ -1,15 +1,38 @@
 import { css, domStyled, FC, jsx } from 'alumina';
-import { appStore } from '~/store';
-import { PcbShapeView } from './PcbShapeView';
+import { colors } from './base';
+import {
+  BottomControlsSection,
+  PcbShapeView,
+  TopControlsSection,
+} from './sections';
 
 export const PageRoot: FC = () => {
   return domStyled(
     <div>
-      <PcbShapeView />
-      <div>
-        <button onClick={appStore.actions.loadKicadPcbFile}>load</button>
+      <h1>PCB Shape Extractor</h1>
+      <TopControlsSection />
+      <div class="main-row">
+        <PcbShapeView />
       </div>
+
+      <BottomControlsSection />
     </div>,
-    css``
+    css`
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+
+      > h1 {
+        font-size: 40px;
+        background: #6c8;
+        color: #fff;
+        padding-left: 10px;
+      }
+
+      > .main-row {
+        border: solid 1px ${colors.panelEdge};
+        padding: 10px;
+      }
+    `
   );
 };
