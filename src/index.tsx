6d61b374ae2523f06f89716bb66be151e4cd8f48
yahiro <yahiro07@users.noreply.github.com>
2022-09-10 18:23:25 +0900

    グローバルスタイルを設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import { jsx, render } from 'alumina';
+import { applyGlobalStyle, jsx, render } from 'alumina';
 import { appStore } from './store';
 import { PageRoot } from './ui';
+import { cssGlobalStyle } from './ui/globalStyle';
 
 async function start() {
   console.log('kicad pcb shape extractor v220910');
   appStore.actions.loadTestData();
+  applyGlobalStyle(cssGlobalStyle);
   render(() => <PageRoot />, document.getElementById('app'));
 }
 
